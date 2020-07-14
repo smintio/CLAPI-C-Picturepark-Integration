6fe51adab9c12802ae6da34fc5343aefcc1b0f2c
Reinhard Holzner <reinhard@smint.io>
2020-07-14 20:16:44 +0200

    Refactoring

--- PictureparkIntegration/Client/Target/Impl/PictureparkSyncTargetImpl.cs
+++ PictureparkIntegration/Client/Target/Impl/PictureparkSyncTargetImpl.cs
@@ -44,7 +44,6 @@ namespace Client.Target.Impl
             return true;
         }
 
-
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task<bool> BeforeGenericMetadataSyncAsync()
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
